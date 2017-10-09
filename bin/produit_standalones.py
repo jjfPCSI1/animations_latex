4e3f3e1411ac5270fbbf14f89fe91c7e1d75b164
Jean-Julien Fleck <jeanjulien.fleck@gmail.com>
2017-10-09 15:39:06 +0200

    Pour les versions seules

---
+++ bin/produit_standalones.py
@@ -0,0 +1,47 @@
+"""
+
+Petit script pour prendre chaque fichier .tex du dossier PS/, le compiler tout 
+seul et le ranger dans le dossier standalones.
+
+A executer depuis la racine du projet github
+
+"""
+
+source_tex = r"""
+
+\documentclass{standalone}
+
+\input{../sty_files/packages.sty}
+\input{../sty_files/macros.sty}
+
+\begin{document}
+
+\input{../CHEMIN}
+
+\end{document}
+
+"""
+
+
+import glob
+import os
+import subprocess
+
+fichiers = glob.glob('PS/*.tex')
+
+os.chdir('standalones')
+# D'abord l'ecriture des fichiers TeX
+FICHIERS_TEX = []
+for fichier in fichiers:
+    PS,nom = fichier.split('/') 
+    fichier_tex = 'standalone_'+nom
+    f = open(fichier_tex,'w')
+    f.write(source_tex.replace('CHEMIN',fichier))
+    f.close()
+    FICHIERS_TEX.append(fichier_tex)
+
+# Puis l'execution
+for fichier_tex in FICHIERS_TEX:
+    subprocess.call(['pdflatex',fichier_tex])
+    subprocess.call(['pdflatex',fichier_tex])
+    
